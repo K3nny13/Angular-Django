6ab19aebf73ccf82c568ead39af0fef993192cef
K3nny13 <kenneth_cogzell@hotmail.com>
2020-06-11 16:40:08 +0200

    Created Angular App

---
+++ django/todoapi/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from datetime import datetime
+
+# Create your models here.
+class Task(models.Model):
+    """
+    Stores a Task
+    """
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=50)
+    created_on = models.DateField(default=datetime.today)
+    due_date = models.DateField(default=datetime.today)
+
+    class Meta:
+        db_table = 'task'
+        ordering = ['id']
+    
+    def __str__(self):
+        return self.title
---
+++ django/todoapi/views.py
@@ -0,0 +1,19 @@
+from rest_framework import generics
+
+from .models import Task
+from .serializers import TaskSerializer
+
+class TaskList(generics.ListCreateAPIView):
+    """
+    Lists and creates tasks.
+    """
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Returns a single Task and allows updates and deletion of a Task.
+    """
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    lookup_url_kwarg = 'task_id'
