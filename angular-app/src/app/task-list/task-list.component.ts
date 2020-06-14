7b2617c2a83f03b4bbb67dbe7dba17a04c4fb62e
K3nny13 <kenneth_cogzell@hotmail.com>
2020-06-14 17:12:22 +0200

    Angular delete Task API service added

--- angular-app/src/app/task-list/task-list.component.ts
+++ angular-app/src/app/task-list/task-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 
 import { ApiService } from '../api.service';
@@ -11,16 +12,57 @@ import { Task } from '../task';
 })
 export class TaskListComponent implements OnInit {
 
+  task_form: FormGroup;
+
   tasks$: Observable<Task[]>;
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService, private form_builder: FormBuilder) { }
 
   ngOnInit(): void {
     this.getTasks();
+
+    this.task_form = this.form_builder.group({
+      title: '',
+      content: ''
+    });
+
+    this.task_form.controls["title"].setValidators([Validators.required]);
+    this.task_form.controls["content"].setValidators([Validators.required]);
+  }
+
+  onSubmit() {
+    // Create the Task.
+    this.apiService.postTask(this.task_form.value)
+      .subscribe(
+        (response) => {
+          console.log(response);
+          this.getTasks();
+        }
+      )
   }
 
   public getTasks() {
     this.tasks$ = this.apiService.getTasks();
+  }
+
+  deleteTask(task_id: number) {
+    this.apiService.deleteTask(task_id)
+      .subscribe(
+        (response) => {
+          console.log(response);
+          this.getTasks();
+        }
+      )
+  }
+
+  updateTask(task) {
+    this.apiService.putTask(task)
+      .subscribe(
+        (response) => {
+          console.log(response);
+          this.getTasks();
+        }
+      )
   }
 
 }
